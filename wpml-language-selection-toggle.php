3a652c0d22cdafe0bf4d21aa14cbfa36853bd9a8
Mathieu Forest <forestmathieu@gmail.com>
2014-11-28 12:57:59 -0500

    test

--- wpml-language-selection-toggle.php
+++ wpml-language-selection-toggle.php
@@ -1,6 +1,6 @@
 <?php
 /*
- * Plugin Name: WPML language selector toggle test
+ * Plugin Name: WPML language selector toggle
  * Version: 1.0
  * Plugin URI: http://mathieuforest.ca/
  * Description: This was develop for custom needs.
